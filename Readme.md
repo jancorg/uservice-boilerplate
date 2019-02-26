eabf9512b88f9701e175ff0e19d0ddf1f9be59ce
Juan Antonio Cortes <jancorg@gmail.com>
2019-02-26 20:54:44 +0100

    index.js and readme

---
+++ Readme.md
@@ -0,0 +1,25 @@
+## micro service boilerplate
+
+just a minimal boilerplate / poc to play with tests
+
+
+### use
+
+```
+npm install
+./index.js -c ./config/config.json
+```
+ or 
+
+```
+docker-compose up
+```
+### test
+
+```
+npm test
+```
+
+### monitoring 
+
+it exposes a prometheus endpoint at `/metrics`
--- index.js
+++ index.js
@@ -1,2 +1,41 @@
 #!/usr/bin/env node
 'use strict'
+
+
+const basicAuth = require('express-basic-auth')
+let server = require('./lib/server')
+const morgan = require('morgan')
+
+const fs = require('fs')// comprobar que es un fichero que existe
+
+
+// load config
+const argv = require( 'argv' )
+argv.option([{
+                name: 'config',
+                short: 'c',
+                type: 'string',
+                description: 'File to load config from. (An absolute path).',
+                example: "'server --config=file' or 'server -c file'"
+            }]
+    )
+
+const args = argv.run()
+
+if (args.options.config && fs.existsSync(args.options.config)) {
+
+    let config = require(args.options.config)
+   
+    // server.use(basicAuth({users: config.users}))
+    server.use(morgan('combined'))
+
+    require('./routes/example')(server)
+    require('./routes/metrics')(server)
+
+    server.listen(config.port)
+    console.log(`Running server on port ${config.port} and config file ${args.options.config}`)
+
+
+} else {
+    argv.help()
+}
